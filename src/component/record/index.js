ff21123f8a2cc4bb2c290c23b247fcf72b3ae55b
Randolf Zhang <zhictory@qq.com>
2019-09-20 17:15:09 +0800

    [feat] add component

---
+++ src/component/record/index.js
@@ -0,0 +1,15 @@
+import "./style.css";
+
+import React, { Component } from 'react';
+
+class Record extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default Record;
