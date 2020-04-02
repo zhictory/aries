2eb4bfaa51b85965c5cb8bd5eda83e099c7cc095
zhangzhensheng <zhangzhensheng@wook.id>
2020-04-02 14:40:12 +0800

    [feat] add router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React from "react";
 import "./App.css";
+import Home from "./component/home";
 import Record from "./component/record";
-// import Rank from "./component/rank";
+import Rank from "./component/rank";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Record />
-      {/* <Rank /> */}
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/record">
+            <Record />
+          </Route>
+          <Route path="/rank">
+            <Rank />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/component/home/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./style.css";
+
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <ul className="home">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/record">Record</Link>
+        </li>
+        <li>
+          <Link to="/rank">Rank</Link>
+        </li>
+      </ul>
+    );
+  }
+}
+
+export default Home;
