d3a1b954d62e0b73d44b34dbd6e7910627bfef17
zhangzhensheng <zhangzhensheng@wook.id>
2021-08-29 18:52:34 +0800

    webpack增加less配置

--- src/components/language/index.tsx
+++ src/components/language/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 // import PropTypes from "prop-types";
-import "./style.css";
+import "./style.less";
 import axios from "axios";
 
 interface IProps {}
